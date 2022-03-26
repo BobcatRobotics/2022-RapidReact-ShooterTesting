50d018a0175809c4ff8b1c3f53ba8fb14361c682
Kashyap Rao <kashyap.bk.rao@gmail.com>
2022-03-26 13:20:36 -0400

    [jem] fixes for ball detection command

--- src/main/java/frc/robot/commands/alignToNearestBall.java
+++ src/main/java/frc/robot/commands/alignToNearestBall.java
@@ -1,11 +1,10 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.RobotContainer;
 import frc.robot.subsystems.Drivetrain;
-
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+import frc.robot.utils.Ball;
 
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
@@ -20,37 +19,19 @@ it to be at. It is not being used anywhere and requires a functional cargo_track
 figure out how to implement the slow zone, so that is not present in this implementation - Arnav
 */
 
-public class AlignToNearestBall extends CommandBase {
+public class alignToNearestBall extends CommandBase {
     
     private Drivetrain drivetrain;
-
-    private JSONParser parser = new JSONParser();
-    private String camJson;
-    private NetworkTable table;
-    private String color;
-
-    private double distance;
-    private double speed = 0.0;
-    private double leftVolts, rightVolts;
-    private double goalX;
-    private double largestRadii;
-    private JSONObject largestRadiiBall;
-    private Timer t = new Timer();
-    private double driveTime = 0.0;
-
-    // arbitrary value of reasonable displacement from center of camera & displacement from center when motor speeds slow down & slowing down scaling factor && vision error on radius of same ball from very similar distances & time allotted per each tiny adjustment turn
-    // TODO: set to appropriate values through testing
-    private final double finalRange = 10;
-    private final double ballRadiiError = 10;
-    private final double time_allotted_per_miniTurn = 0.1;
     private double angle = -1000;
+    private RobotContainer m_rContainer;
 
     // UPDATE: 10:43 pm currently and I can't get slow zone to work, so I'm going to leave it incomplete for now - Arnav
     //private final double slowZone = 20;
     //private final double slowScalingFactor = 0.5;
 
-    public AlignToNearestBall(Drivetrain dt) {
+    public alignToNearestBall(Drivetrain dt, RobotContainer r) {
         drivetrain = dt;
+        m_rContainer = r;
     }
 
     @Override
@@ -59,14 +40,14 @@ public class AlignToNearestBall extends CommandBase {
 
     @Override
     public void execute() {
-        Ball closestBall = m_robotContainer.getClosestBall();
+        Ball closestBall = m_rContainer.getClosestBall();
 
         if (closestBall != null) {
-            angle = closestBall.get Angle();
-            double drivePower = (angle/(Constants.BallCameraConstants.cameraFOV/2)/3);
-            diveTrain.drive(drivePower,drivePower);
+            angle = closestBall.getAngle();
+            double drivePower = angle / 3;
+            drivetrain.drive(drivePower,drivePower);
         } else {
-            diveTrain.drive(3,3);
+            drivetrain.drive(2.5,2.5);
         }
 
     }
@@ -79,8 +60,10 @@ public class AlignToNearestBall extends CommandBase {
     
     @Override
     public boolean isFinished() {
-        if(angle = 0) {
+        if(angle == 0) {
             return true;
+        } else {
+            return false;
         }
     }
 }
